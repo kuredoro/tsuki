1d7d2e950132cc857f97ea38b4e43ca37cc6ec52
しずえ <66309775+kuredoro@users.noreply.github.com>
2020-10-10 17:37:59 +0300

    fsd now starts the heartbeat service.

--- heart.go
+++ heart.go
@@ -24,7 +24,7 @@ func (p *HTTPPoller) Poll() {
     _, err := http.Get(p.address)
 
     if err != nil {
-        log.Printf("warning: could sent hertbeat to %s", p.address)
+        log.Printf("warning: couldn't send hertbeat to %s", p.address)
     }
 }
 
