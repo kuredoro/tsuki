94b8940f98fd0cd26172ce2eb9056535e1107a43
TheSharpOwl <ziadxkabakibi@hotmail.com>
2020-10-12 21:02:45 +0300

    Use third-party library for CLI apps. Persistent NS address

--- cmd/tsuki/main.go
+++ cmd/tsuki/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -10,8 +9,19 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"strings"
+
+	"github.com/urfave/cli/v2"
 )
+
+const (
+    TempNS = "tsuki.ns"
+    TempCwd = "tsuki.cwd"
+)
+
+const NSCLIENTPORT = ":7070"
+
+var ns string
+var cwd string
 
 type ChunkMessage struct {
 	ChunkID   string `json:"chunkID"`
@@ -19,7 +29,7 @@ type ChunkMessage struct {
 }
 
 type ClientMessage struct {
-	Status  int         `json:"-"`
+	Status  int            `json:"-"`
 	Message string         `json:"message"`
 	Objects []string       `json:"objects"`
 	Token   string         `json:"token"`
@@ -27,91 +37,152 @@ type ClientMessage struct {
 }
 
 type NSClientConnector struct {
-	nsAddr string
+	NSAddr string
 }
 
 func UnmarshalNSResponse(response *http.Response) (msg *ClientMessage) {
 	buf := &bytes.Buffer{}
 	io.Copy(buf, response.Body)
 
-    msg = &ClientMessage{ Status: response.StatusCode }
+	msg = &ClientMessage{Status: response.StatusCode}
 
-    if err := json.Unmarshal(buf.Bytes(), &msg); err != nil {
-        log.Printf("warning: could not unmarshal response %q, %v", buf, err)
-    }
-    
-    return
+	if err := json.Unmarshal(buf.Bytes(), &msg); err != nil {
+		log.Printf("warning: could not unmarshal response %q, %v", buf, err)
+	}
+
+	return
+}
+
+func (conn *NSClientConnector) GetNS(cmd, path string) (*http.Response, error) {
+	addr := fmt.Sprintf("http://%s%s/%s?address=%s", conn.NSAddr, NSCLIENTPORT, cmd, path)
+	return http.Get(addr)
 }
 
 func (conn *NSClientConnector) Ls(path string) ([]string, error) {
 	log.Print("ls ", path)
 
-	addr := fmt.Sprintf("http://%s/%s?address=%s", conn.nsAddr, "ls", path)
-	resp, err := http.Get(addr)
+	resp, err := conn.GetNS("ls", path)
 	if err != nil {
 		log.Printf("error: ls GET error, %v", err)
-		return nil, fmt.Errorf("could not send request, %v", err)
+		return nil, fmt.Errorf("send request: %v", err)
 	}
 
-    msg := UnmarshalNSResponse(resp)
+	msg := UnmarshalNSResponse(resp)
 
-    if msg.Status != http.StatusOK {
-        return nil, fmt.Errorf(msg.Message)
-    }
+	if msg.Status != http.StatusOK {
+		return nil, fmt.Errorf(msg.Message)
+	}
 
 	log.Printf("Received message: %#v", msg)
 
 	return msg.Objects, nil
 }
 
+
+func saveCwd() {
+}
+
+func loadFromTemp(name string) string {
+    filename := path.Join(os.TempDir(), name)
+    if _, err := os.Stat(filename); os.IsNotExist(err) {
+        return ""
+    }
+
+    file, err := os.Open(filename)
+    if err != nil {
+        log.Printf("warning: could not open temp file, %v", err)
+        return ""
+    }
+    defer file.Close()
+
+    buf := &bytes.Buffer{}
+    io.Copy(buf, file)
+
+    return buf.String()
+}
+
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Printf("Usage: tsuki hostaddr:port\n")
-		return
-	}
+
+    ns := loadFromTemp(TempNS)
 
 	conn := &NSClientConnector{
-		nsAddr: os.Args[1],
+		NSAddr: ns,
 	}
+    log.Print(ns)
 
-    curPath := "/"
-
-	_, err := conn.Ls(curPath)
-	if err != nil {
-		fmt.Println("could not connect to NS, ", err)
-		return
-	}
-
-    scanner := bufio.NewScanner(os.Stdin)
-    for {
-        fmt.Printf("%s $ ", curPath)
-
-        scanner.Scan()
-
-        cmd := strings.Split(scanner.Text(), " ")
-
-        if len(cmd) == 0 {
-            continue
-        }
-
-        switch cmd[0] {
-        case "ls":
-            lsDir := curPath
-            if len(cmd) == 2 {
-                lsDir = path.Clean(cmd[1])
-            }
-
-            objects, err := conn.Ls(lsDir)
-            if err != nil {
-                fmt.Printf("error: %v\n", err)
-                continue
-            }
-
-            fmt.Println(strings.Join(objects, "\n"))
-        case "exit", "quit":
-            return
-        default:
-            fmt.Printf("error: unknown command %q\n", cmd[0])
-        }
+    cwd = loadFromTemp(TempCwd)
+    if cwd == "" {
+        cwd = "/"
+        saveCwd()
     }
+
+	app := &cli.App{
+		Name:  "tsuki",
+		Usage: "a CLI interface to tsukiFS distributed file system",
+        Commands: []*cli.Command {
+            {
+                Name: "connect",
+                Usage: "Probe and remember name server for future calls",
+                Action: func(c *cli.Context) error {
+                    conn.NSAddr = c.Args().First()
+
+                    _, err := conn.Ls("/")
+                    if err != nil {
+                        return fmt.Errorf("%v", err)
+                    }
+
+                    file, err := os.Create(path.Join(os.TempDir(), TempNS))
+                    if err != nil {
+                        fmt.Printf("warning: could not create NS temp file: %v", err)
+                        return nil
+                    }
+                    defer file.Close()
+
+                    fmt.Fprint(file, conn.NSAddr)
+
+                    return nil
+                },
+            },
+        },
+	}
+
+    err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+    /*
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("%s $ ", curPath)
+
+		scanner.Scan()
+
+		cmd := strings.Split(scanner.Text(), " ")
+
+		if len(cmd) == 0 {
+			continue
+		}
+
+		switch cmd[0] {
+		case "ls":
+			lsDir := curPath
+			if len(cmd) == 2 {
+				lsDir = path.Clean(cmd[1])
+			}
+
+			objects, err := conn.Ls(lsDir)
+			if err != nil {
+				fmt.Printf("error: %v\n", err)
+				continue
+			}
+
+			fmt.Println(strings.Join(objects, "\n"))
+		case "exit", "quit":
+			return
+		default:
+			fmt.Printf("error: unknown command %q\n", cmd[0])
+		}
+	}
+    */
 }
